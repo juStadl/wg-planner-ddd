064a65934cbe411d2c67fec6b5f4d308f939d3e5
Julian Stadler <jstadler@testo.de>
2024-03-05 11:08:45 +0100

    initial commit

---
+++ plugins/src/main/java/de/dhbw/softwareengineering/WgPlannerApplication.java
@@ -0,0 +1,12 @@
+package de.dhbw.softwareengineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgPlannerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WgPlannerApplication.class, args);
+    }
+
+}
