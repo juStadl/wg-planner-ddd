50f13ea10c70dd28bddfe2b3c843a2020a8abe26
Julian Stadler <jstadler@testo.de>
2024-04-09 14:52:13 +0200

    implement unit tests for PersonServic

--- domains/src/main/java/de/dhbw/softwareengineering/repositories/PersonRepository.java
+++ domains/src/main/java/de/dhbw/softwareengineering/repositories/PersonRepository.java
@@ -1,11 +1,13 @@
 package de.dhbw.softwareengineering.repositories;
 
 import de.dhbw.softwareengineering.entities.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface PersonRepository {
     List<Person> findAll();
     Person save(Person person);
