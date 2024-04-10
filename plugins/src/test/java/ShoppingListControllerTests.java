3f265e52bfaee73f1501bf6b9bcdc72efae030cb
Julian Stadler <jstadler@testo.de>
2024-04-10 19:36:06 +0200

    cleared controllertestclass

--- plugins/src/test/java/ShoppingListControllerTests.java
+++ plugins/src/test/java/ShoppingListControllerTests.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.dhbw.softwareengineering.PersonService;
+import de.dhbw.softwareengineering.plugins.persistence.PersonRepositoryBridge;
 import de.dhbw.softwareengineering.plugins.rest.PersonController;
 import de.dhbw.softwareengineering.repositories.PersonRepository;
 import de.dhbw.softwareengineering.representations.PersonRepresentation;
@@ -24,44 +25,6 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = {PersonService.class, PersonController.class, })
 class ShoppingListControllerTests {
-
-   private MockMvc mockMvc;
-
-   @Autowired
-   PersonRepository personRepository;
-
-   @Mock
-   private PersonService personService;
-   private PersonController personController;
-   private ObjectMapper objectMapper;
-
-   @BeforeEach
-   void init(){
-       MockitoAnnotations.openMocks(this);
-       personController = new PersonController(personService);
-       mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
-       objectMapper = new ObjectMapper();
-   }
-
-   @Test
-   void  PersonController_Create_ReturnsCreatedPersonRepresentation() throws Exception {
-       PersonRepresentation personRepresentation = PersonRepresentation.builder()
-               .id(UUID.randomUUID())
-               .name(new Name("firstname", "lastname"))
-               .address(new Address("street", "city", "zipCode"))
-               .birthDate(LocalDate.now())
-               .gender(Gender.MALE)
-               .build();
-
-       given(personService.create(ArgumentMatchers.any(PersonRepresentation.class))).willReturn(personRepresentation);
-
-       mockMvc.perform(post("/person/create")
-               .contentType(MediaType.APPLICATION_JSON_VALUE)
-               .content(objectMapper.writeValueAsString(personRepresentation)))
-               .andExpect(status().isCreated());
-   }
-
 
 }
